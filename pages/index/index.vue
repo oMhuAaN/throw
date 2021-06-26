f6dc231f7a166349442d53a51c5e1f2a8d868c58
haibin@k590s-i7 <894457272@qq.com>
2021-06-26 21:21:23 +0800

    234234

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,13 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="lsdfho">
+		<view class="dsfds3">
+			<view class="ldjfl3fd">{{value}}</view>
 		</view>
+		<view class="flkdjfe" @click="handleClick_1">
+			转
+		</view>
+		<audio style="text-align: left" :src="current.src" :poster="current.poster" :name="current.name"
+			:author="current.author" :action="audioAction" controls></audio>
 	</view>
 </template>
 
@@ -11,42 +15,59 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				value: 6,
+				poster: 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/7fbf26a0-4f4a-11eb-b680-7980c8a877b8.png',
+				name: '致爱丽丝',
+				author: '暂无',
+				src: 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3'
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			handleClick_1() {
+				// alert('xxdfsdf')
+				this.value = Math.ceil(Math.random() * 6);
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
+	.dsfds3 {
+		font-weight: 900;
+		font-size: 200rpx;
+		color: #fff;
+		margin-top: 300rpx;
+		padding: 20rpx 50rpx;
+		border: 3px solid red;
+		border-radius: 10rpx;
+	}
+
+	.flkdjfe {
+		font-size: 200rpx;
+		color: #1AA034;
+		margin-top: 200rpx;
+		cursor: pointer;
+		padding: 20rpx 50rpx;
+		border: 3px solid yellow;
+		border-radius: 10rpx;
+	}
+
+	.lsdfho {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
+		background-color: #070F17;
+		height: 100vh;
+		position: fixed;
+		left: 0rpx;
+		top: 0rpx;
+		width: 750rpx;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.bb {
+		border: 1px solid red;
 	}
 </style>
